aaf79c304e0c6c4d8ed8bd52a73c3b55da6b55e0
wenjunma <wenjunma001@gmail.com>
2018-09-07 09:30:03 -0700

    add all

---
+++ src/components/Playgroud.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+  {{msg}}
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'playgroud',
+    data () {
+      return {
+        msg: 'playground'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
